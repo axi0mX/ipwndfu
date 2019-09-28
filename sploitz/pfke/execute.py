af7ce084a9df7e25280bf2f421bfcc914ab48ab7
6100m <31662463+6100m@users.noreply.github.com>
2019-09-28 15:42:13 -0600

    Finish executor for pfke exploit

--- sploitz/pfke/execute.py
+++ sploitz/pfke/execute.py
@@ -1,10 +1,28 @@
-///Sourced from https://stackoverflow.com/questions/18762621/can-we-use-c-code-in-python and https://stackoverflow.com/questions/3987041/run-function-from-the-command-line
-import sys
-import pfke
-result == pfke.pwn()
-if result == pfke.pwn()
+///Sourced from https://stackoverflow.com/questions/18762621/can-we-use-c-code-in-python 
+///and https://stackoverflow.com/questions/3987041/run-function-from-the-command-line
+///and https://stackoverflow.com/questions/73663/terminating-a-python-script
+///and https://www.w3schools.com/python/python_conditions.asp
+import sys as a
+import pfke as b
+from sys import exit as c
+from pfke import pwn as d
+from sys import *
+from pfke import *
+result == b.d()
+if result == b.d()
     state == yesletspwn
     if state == yesletspwn
-        pfke.pwn()
-        goto terminate
-        
+        b.d()
+        a.c()
+        quit()
+        raise SystemExit
+    else:
+        print 'Error Code 001'
+        a.c()
+        quit()
+        raise SystemExit
+else:
+    print 'Error Code 002
+    a.c)_
+    quit()
+    raise SystemExit
