d2befd94fc19dc386c13ff1ffda15379154eb9cc
6100m <31662463+6100m@users.noreply.github.com>
2019-09-28 16:36:32 -0700

    Finalize, and kill method 2 from joblist

---
+++ pfke.py
@@ -0,0 +1,11 @@
+///Sourced from https://stackoverflow.com/questions/73663/terminating-a-python-script
+import subprocess as a
+import sys as b
+from sys import exit as c
+from sys import *
+from subprocess import call as d
+from subprocess import *
+a.d(["python", "sploitz/dir.py"])
+b.c()
+quit()
+raise SystemExit
--- pfke_method1.py
+++
@@ -1,2 +0,0 @@
-import subprocess as a
-a.call(["python", "sploitz/pfke/execute.py"])
