f23183da54ae8b8c31efe42c24ce06e63a1476b8
6100m <31662463+6100m@users.noreply.github.com>
2019-09-28 14:51:42 -0600

    need to finish this execution code later

---
+++ sploitz/pfke/execute.py
@@ -0,0 +1,10 @@
+///Sourced from https://stackoverflow.com/questions/18762621/can-we-use-c-code-in-python and https://stackoverflow.com/questions/3987041/run-function-from-the-command-line
+import sys
+import pfke
+result == pfke.pwn()
+if result == pfke.pwn()
+    state == yesletspwn
+    if state == yesletspwn
+        pfke.pwn()
+        goto terminate
+        
