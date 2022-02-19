9de7f5753b3686476cd963779458334657f66c7e
adam <adamhamdi31@gmail.com>
2022-02-19 12:38:48 -0600

    [usbexec] Add T7000 ExecConfig (#48)

--- src/ipwndfu/usbexec.py
+++ src/ipwndfu/usbexec.py
@@ -68,6 +68,14 @@ configs = [
     ),
     ExecConfig(
         (
+            "SecureROM for t7000si, Copyright 2013, Apple Inc.",
+            "RELEASE",
+            "iBoot-1992.0.0.1.19"
+        ),
+        aes_crypto_cmd=0x10000DA90
+    ),
+    ExecConfig(
+        (
             "SecureROM for s8000si, Copyright 2007-2014, Apple Inc.",
             "RELEASE",
             "iBoot-2234.0.0.3.3",
